a5018cc0169b4aa653aa49cccb197c32431f331b
Robert <robert.ios.developer@gmail.com>
2022-05-22 16:05:29 -0600

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Robert on 5/22/22.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
